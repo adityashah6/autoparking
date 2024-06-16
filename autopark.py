fbd0701755bc7685e2bcd8a28cba7e9585c3d3e7
adityashah6 <60235219+adityashah6@users.noreply.github.com>
2024-06-15 22:00:28 -0400

    Update autopark.py

--- autopark.py
+++ autopark.py
@@ -167,7 +167,7 @@ try:
     time.sleep(2)  # Wait for the calendar to appear
 
     # Calculate the next day again for the end date
-    if today_date == last day of month:
+    if today_date == last_day_of_month:
         # Click on the next button to go to the next month
         next_button = wait_for_element(driver, By.CLASS_NAME, 'ui-datepicker-next', timeout=30)
         next_button.click()
@@ -260,7 +260,7 @@ try:
     time.sleep(.5)  # Small delay to ensure the element is in view
 
     # Ensure the submit button is visible and clickable
-    if submit_button.is_displayed() and submit_button.is_enabled():
+    if submit_button.is_displayed() and submit_button is_enabled():
         try:
             submit_button.click()
             logger.info("Submit button clicked successfully.")
